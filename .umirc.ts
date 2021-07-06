25c311f528f426e809297775d30d1e3a9bfcf858
linzhenfang <linzhenfang@pinming.cn>
2021-07-06 11:01:13 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,53 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  proxy: {
+    '/api/app1': {
+      target: 'http://localhost:8001',
+      changeOrigin: true,
+    },
+    '/api/app2': {
+      target: 'http://localhost:8002',
+      changeOrigin: true,
+    },
+    '/api/app3': {
+      target: 'http://localhost:8003',
+      changeOrigin: true,
+    },
+  },
+  qiankun: {
+    master: {
+      apps: [
+        {
+          name: 'app1', // 唯一 id
+          entry: '//localhost:7001', // html entry
+        },
+      ],
+    },
+  },
+  routes: [
+    {
+      path: '/',
+      // component: '@/pages/index',
+      component: '../layouts/index.js',
+      routes: [
+        // {
+        //   path: '/app1',
+        //   component: './subContainer',
+        // },
+        // {
+        //   path: '/app2',
+        //   component: './subContainer',
+        // },
+        {
+          path: '/',
+          component: './index',
+        },
+      ],
+    },
+  ],
+  fastRefresh: {},
+});
---
+++ src/pages/subContainer.tsx
@@ -0,0 +1,10 @@
+import styles from './index.less';
+import { MicroApp } from 'umi';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <MicroApp name="app1" />
+    </div>
+  );
+}
